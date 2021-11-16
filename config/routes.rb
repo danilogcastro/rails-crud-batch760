8aea6006f671360804fb839bf4ff847c09f38fc4
Danilo Castro <danilogcastro@hotmail.com>
2021-11-16 10:48:18 -0300

    restaurant app done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # VERB 'path', to: 'controller#action'
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new'
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  # patch 'restaurants/:id', to: 'restaurants#update'
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+
+  resources :restaurants
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding..."
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    rating: rand(1..5)
+  )
+  restaurant.save!
+  puts "#{restaurant.name} saved to database!"
+end
+
+puts "All done!"
